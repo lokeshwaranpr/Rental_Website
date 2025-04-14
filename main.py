2004b83a08c9b6a7f8feed322f482700e92e6495
lokeshwaranpr <lokeshwaranpr@gmail.com>
2025-04-14 22:42:30 +0530

    new changes

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+
+
 
 
 app = FastAPI()
@@ -17,6 +22,18 @@ class Item(BaseModel):
     image_url: Optional[str] = None
     location: str
     available: bool
+while True:
+
+    try:
+        conn = psycopg2.connect(host="localhost", database="fastapi", user="postgres", password="password", cursor_factory= RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection successful")
+        break
+    except Exception as e:
+        print("Database Connection failed")
+        print("Error",e)
+        time.sleep(2)
+
 
 @app.get("/")
 def root():
@@ -25,3 +42,9 @@ def root():
 @app.post("/createpost")
 def create_item():
     return {"Message":"Post created successfully"}
+
+@app.get("/getposts")
+def get_item():
+    cursor.execute("SELECT * FROM item")
+    items = cursor.fetchall()
+    return {"posts":items}
